4f1c47720e43c97c119deb1432202537a7266378
wenyi <17621889147@163.com>
2021-09-26 11:10:14 +0800

    多线程实现，增加线程退出

--- mythreadpool.cc
+++ mythreadpool.cc
@@ -1,20 +1,28 @@
 #include"mythreadpool.hpp"
 #include  <unistd.h>
 int main(){
-  mythreadpool *tp=new mythreadpool();
-  tp->threadinit();
-  while(1){
-    //sleep(1);
-    int x=rand()%10+1;
-    int y=rand()%20+1;
+	mythreadpool *tp = new mythreadpool();
+	tp->threadinit();
+	int count = 10;
+	while (1){
+		//sleep(1);
+		int x = rand() % 10 + 1;
+		int y = rand() % 20 + 1;
 
-    Task t(x,y);
-    //put里有加锁，不需要加锁了
-    tp->Put(t);
-    //std::cout<<"put Task done..."<<std::endl;
-  }
-  delete tp;
+		Task t(x, y);
+		//put里有加锁，不需要加锁了
+		tp->Put(t);
+		std::cout << "put Task done..." << std::endl;
+		count--;
+		if (count == 0){
+			break;
+		}
+	}
+
+	//sleep(3);
+	tp->ThreadQuit();
+	delete tp;
 
 
-  return 0;
+	return 0;
 }
--- mythreadpool.hpp
+++ mythreadpool.hpp
@@ -1,101 +1,136 @@
 #pragma once 
 #include <iostream>
 #include <queue>
+#include <unistd.h>
 #include <pthread.h>
 #define NUM 5
 struct Task{
-  Task(){};
-  Task(int x, int y){
-    _x=x;
-    _y=y;
-  }
-  ~Task(){};
-  int _x;
-  int _y;
-  int Add(){
-    return _x + _y;
-  }
+	Task(){};
+	Task(int x, int y){
+		_x = x;
+		_y = y;
+	}
+	~Task(){};
+	int _x;
+	int _y;
+	int Add(){
+		return _x + _y;
+	}
 
 };
 
 class mythreadpool{
-  private:
-    void ThreadLock(){
-      pthread_mutex_lock(&_lock);
-    }
-    void ThreadUnlock(){
-      pthread_mutex_unlock(&_lock);
-    }
-    void ThreadWait(){
-      pthread_cond_wait(&_cond, &_lock);
-    }
-    void ThreadSignal(){
-      pthread_cond_signal(&_cond);
-    }
-    bool IsEmpty(){
-      return _q.empty();
-    }
+private:
+	void ThreadLock(){
+		pthread_mutex_lock(&_lock);
+	}
+	void ThreadUnlock(){
+		pthread_mutex_unlock(&_lock);
+	}
+	void ThreadWait(){
+		pthread_cond_wait(&_cond, &_lock);
+	}
+	void ProductorWait(){
+		pthread_cond_wait(&_pcond, &_lock);
+	}
+	void ProductorSignal(){
+		pthread_cond_signal(&_pcond);
+	}
+	void ThreadSignal(){
+		pthread_cond_signal(&_cond);
+	}
+	void ThreadSignalAll(){
+		pthread_cond_broadcast(&_cond);
+	}
+	bool IsEmpty(){
+		return _q.empty();
+	}
+	void ThreadExit(){
+		_numthread--;
+		printf("%lu is exit\n", pthread_self());
+		ProductorSignal();
+		pthread_exit(0);
+	}
 
-  public:
-    mythreadpool(size_t num = NUM)
-    :_numthread(num)
-    {}
-    //成员函数，由this指针作为参数，
-    //线程函数，只能有一个参数，所以要加static
-    static void *handler(void *arg){
-      mythreadpool *p_this = (mythreadpool *)arg;
-      while(1){
-        p_this->ThreadLock();
-        while(p_this->IsEmpty()){
-          p_this->ThreadWait();
-        }
-        Task t;
-        p_this->Get(t);
-        p_this->ThreadUnlock();
-        int res=0;
-        res = t.Add();
-        std::cout<<pthread_self()<<":"<<t._x<<"+"<<t._y<<"="<<res<<std::endl;
+public:
+	mythreadpool(size_t num = NUM)
+		:_numthread(num)
+		, _quit(false)
+	{}
+	//成员函数，由this指针作为参数，
+	//线程函数，只能有一个参数，所以要加static
+	static void *handler(void *arg){
+		mythreadpool *p_this = (mythreadpool *)arg;
+		while (1){
+			usleep(100);
+			p_this->ThreadLock();
+			while (!(p_this->_quit) && (p_this->IsEmpty())){
+				p_this->ThreadWait();
+			}
+			if (p_this->_quit&&p_this->IsEmpty()){
+				p_this->ThreadUnlock();
+				p_this->ThreadExit();
+			}
+			Task t;
+			p_this->Get(t);
+			p_this->ThreadUnlock();
+			int res = 0;
+			res = t.Add();
+			std::cout << pthread_self() << ":" << t._x << "+" << t._y << "=" << res << std::endl;
 
-      }
+		}
 
-    }
-    void threadinit(){
-      pthread_mutex_init(&_lock, nullptr);
-      pthread_cond_init(&_cond, nullptr);
-      pthread_t td[_numthread]; 
-      size_t i=0;
-      for(i=0; i<_numthread; i++){
-        pthread_create(td+i, nullptr, handler, (void *)this);
-      }
-      for(i=0; i<_numthread; i++){
-        pthread_detach(td[i]);
-      }
+	}
+	void threadinit(){
+		pthread_mutex_init(&_lock, nullptr);
+		pthread_cond_init(&_cond, nullptr);
+		pthread_cond_init(&_pcond, nullptr);
+		pthread_t td[_numthread];
+		size_t i = 0;
+		for (i = 0; i<_numthread; i++){
+			pthread_create(td + i, nullptr, handler, (void *)this);
+		}
+		for (i = 0; i<_numthread; i++){
+			pthread_detach(td[i]);
+		}
 
 
-    }
-    void Get(Task& out){
-     Task *t = _q.front();
-     _q.pop();
-     out=*t;
-    }
+	}
+	void Get(Task& out){
+		Task *t = _q.front();
+		_q.pop();
+		out = *t;
+	}
 
-    void Put(Task& in){
-      ThreadLock();
-      _q.push(&in);
-      ThreadUnlock();
-      ThreadSignal();
-    }
+	void Put(Task& in){
+		ThreadLock();
+		_q.push(&in);
+		ThreadUnlock();
+		ThreadSignal();
+	}
 
-    ~mythreadpool(){
-      pthread_mutex_destroy(&_lock);
-      pthread_cond_destroy(&_cond);
+	~mythreadpool(){
+		pthread_mutex_destroy(&_lock);
+		pthread_cond_destroy(&_cond);
+		pthread_cond_destroy(&_pcond);
 
-    }
-  private:
-    std::queue<Task*> _q;
-    size_t _numthread;//线程个数
-    pthread_mutex_t _lock;
-    //只能消费者等，生产者不仅要放任务，还需要受理用户请求
-    pthread_cond_t _cond;
+	}
+	//线程退出
+	void ThreadQuit(){
+		ThreadLock();
+		_quit = true;
+		ThreadSignalAll();
+		while (_numthread != 0){
+			ProductorWait();
+		}
+		ThreadUnlock();
+	}
+private:
+	std::queue<Task*> _q;//任务队列
+	size_t _numthread;//线程个数
+	pthread_mutex_t _lock;//锁
+	pthread_cond_t _cond;//消费者在此等待
+	pthread_cond_t _pcond;//生产者在此等待
 
+	bool _quit;//是否退出
 };
